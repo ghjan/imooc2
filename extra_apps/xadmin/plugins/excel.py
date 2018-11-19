323b50a11c2ac33f1e8b42045a2dc9d49ac38e00
jian.zhang <cajan2@163.com>
2018-11-19 10:58:35 +0800

    fix an issue:render_to_string unexpected keyword argument context_instance

--- extra_apps/xadmin/plugins/excel.py
+++ extra_apps/xadmin/plugins/excel.py
@@ -17,12 +17,14 @@ from django.template import loader
 class ListImportExcelPlugin(BaseAdminPlugin):
     import_excel = False
 
+    # 返回True的时候，加载本插件
     def init_request(self, *args, **kwargs):
         return bool(self.import_excel)
 
     def block_top_toolbar(self, context, nodes):
-        nodes.append(
-            loader.render_to_string('xadmin/excel/model_list.top_toolbar.import.html', context_instance=context))
+        nodes.append(loader.render_to_string(
+            'xadmin/excel/model_list.top_toolbar.import.html', context=context.dicts[1]
+        ))
 
 
 xadmin.site.register_plugin(ListImportExcelPlugin, ListAdminView)
